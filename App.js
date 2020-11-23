8d01ea9059aff2ae701cae8317d1dc0e041f5d3b
Vinnie-ndesh <Vinnie-ndesh@users.noreply.github.com>
2020-11-23 14:07:24 +0300

    firebase auth

--- App.js
+++ App.js
@@ -2,6 +2,25 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyBn3x9Oc7UEc2yv7NyI-KJyc9Wv_RecUb4",
+    authDomain: "react-chatapp-6df18.firebaseapp.com",
+    databaseURL: "https://react-chatapp-6df18.firebaseio.com",
+    projectId: "react-chatapp-6df18",
+    storageBucket: "react-chatapp-6df18.appspot.com",
+    messagingSenderId: "398635131172",
+    appId: "1:398635131172:web:0f3eae529611a2f31713b1",
+    measurementId: "G-4C7XFDF4EL"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+
 export default function App() {
   return (
     <View style={styles.container}>
---
+++ screens/homepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from "react-native-web";
+
+const Homepage = () => {
+    return (
+        <View>
+        <Text>welcome to the home page</Text>
+        </View>
+    );
+};
+
+export default Homepage;
